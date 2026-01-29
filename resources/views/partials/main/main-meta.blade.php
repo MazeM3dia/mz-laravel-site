be51f40908094c35d8319f56e17a5d7c9425092e
MazeM3dia <info@mazem3dia.net>
2026-01-29 14:49:03 +0800

    Adding new font

--- resources/views/partials/main/main-meta.blade.php
+++ resources/views/partials/main/main-meta.blade.php
@@ -8,7 +8,8 @@
 
 <title>{{ config('app.name', 'Laravel') }}</title>
 
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
